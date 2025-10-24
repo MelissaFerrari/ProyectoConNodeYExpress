de524d86ea3b07cb148fd408f5f3cf3379657481
Melissa Ferrari <melissa.ferrari.v@gmail.com>
2025-10-24 20:54:35 -0300

    me falto instalación de dotenv para leer las variables

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+require('dotenv').config(); // importante si usás variables de entorno
 
 // Puerto
 const PORT = 3000;
@@ -15,7 +16,13 @@ app.get('/', (req, res) => {
   res.send('Servidor Express conectado a MySQL con Sequelize 🚀');
 });
 
-// Iniciar el servidor
-app.listen(PORT, () => {
+// Iniciar el servidor y probar conexión a la base de datos
+app.listen(PORT, async () => {
+  try {
+    await sequelize.authenticate(); // 🔹 intenta conectar con la BD
+    console.log('✅ Conexión exitosa con la base de datos MySQL');
+  } catch (error) {
+    console.error('❌ Error al conectar con la base de datos:', error);
+  }
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
