fdb48fa0e388ef5848f01d542a5caaf888539efa
MelissaFerrari <melissa.ferrari.v@gmail.com>
2026-02-25 18:17:43 -0300

    fix sequelize.js para usar variables de entorno

--- src/models/sequelize.js
+++ src/models/sequelize.js
@@ -1,8 +1,9 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('discord_sencillo', 'root', 'root', {
-  host: 'localhost',
-  dialect: 'mysql', // 👈 ESTE CAMPO ES OBLIGATORIO
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT,
   logging: false,
 });
 
