1b8689060b11d2d088a3e41ede17bf97b7478262
MelissaFerrari <melissa.ferrari.v@gmail.com>
2026-02-25 18:27:52 -0300

    configure cors to allow all origins

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ const PORT = process.env.PORT || 3000;
 const sequelize = require('./config/database');
 
 // 🔥 Middleware para parsear JSON (TIENE QUE IR ARRIBA)
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
